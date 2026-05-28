960d100bca9629ce0ce2e46baa5ff31c985f79ce
aneelakheli <kanxoakheli22@gmail.com>
2026-05-28 08:29:50 +0545

    [DOC] understanding of the datatype in python

---
+++ datatypes/datatype.py
@@ -0,0 +1,26 @@
+print("Hello world")
+
+first_name = "Madan"
+print(first_name)
+
+
+
+num_1 = 10 #integer
+floating_number = 20.89000000
+complex_number = 5
+
+
+first_name= "aneeakheli"  #string
+is_capable =23
+if is_capable >12:
+    True #boolean
+
+print(type(first_name))
+
+print(type(num_1))
+
+
+
+
+# num  = int(input("Enter a number: "))
+# print("The number you entered is:", num)
